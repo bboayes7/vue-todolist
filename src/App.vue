07843bfc51fdccce02584029fbfda44af1884d70
Barry <barryboayes17@gmail.com>
2019-04-10 13:54:55 -0700

    implemented api requests from jsonplaceholder

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
+import axios from 'axios';
 
 export default {
   name: 'app',
@@ -21,32 +22,29 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: 'One',
-          completed: false
-        },
-        {
-          id: 2,
-          title: 'Two',
-          completed: true
-        },
-        {
-          id: 3,
-          title: 'Three',
-          completed: false
-        }
-      ]
+      todos: []
     }
   },
   methods: {
     deleteTodo(id){
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => this.todos = this.todos.filter(todo => todo.id !== id))//eslint-disable-next-line
+        .catch(err => console.log(err));
     },
     addTodo(newTodo){
-      this.todos = [...this.todos, newTodo];
+      const {title, completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data]) //eslint-disable-next-line
+        .catch(err => console.log(err));
     }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(res => this.todos = res.data)  //eslint-disable-next-line
+    .catch(err => console.log(err));
   }
 }
 </script>
